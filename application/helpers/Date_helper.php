3c727d338eb027abb618b942d23750ea433edd69
PanduAldi <pratamapandu652@gmail.com>
2016-11-02 01:30:55 +0700

    pendaftaran & pelanggan(exclude edit)

---
+++ application/helpers/Date_helper.php
@@ -0,0 +1,39 @@
+<?php  
+
+/**
+ * Function Name
+ *
+ * Function description
+ *
+ * @access	public
+ * @param	type	name
+ * @return	type	
+ */
+ 
+if (! function_exists('show_month'))
+{
+	function show_month($month = NULL)
+	{
+		if ($month == NULL) {
+			return FALSE;
+		}
+
+		$month_name = array(
+						1 => 'Januari',
+						2 => 'Februari',
+						3 => 'Maret',
+						4 => 'April',
+						5 => 'Mei',
+						6 => 'Juni',
+						7 => 'Juli',
+						8 => 'Agustus',
+						9 => 'September',
+						10 => 'Oktober',
+						11 => 'November',
+						12 => 'Desember'
+					);
+		return $month_name[$month];
+	}
+}
+
+?>
